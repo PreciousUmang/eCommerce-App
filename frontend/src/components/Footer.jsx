3e9b88028a27d90e9c42298da8804593f6f93765
Precious Umang <umang@technologist.com>
2025-01-08 02:28:56 +0530

    - Created Header and Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+
+function Footer() {
+    const currentYear = new Date().getFullYear()
+
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="py-3 text-center">
+                        <p>Chahat Collections &copy; {currentYear}</p></Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;
