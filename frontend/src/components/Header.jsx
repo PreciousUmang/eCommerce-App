38fc60bc0559960ce9661cb38d436ce6bc0d8ecf
Precious Umang <umang@technologist.com>
2025-01-24 19:36:07 +0530

    - Added Card Item Numbers and cleared Cart Slice code

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,14 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import { useSelector } from 'react-redux';
+
 
 const Header = () => {
+
+    const { cartItems } = useSelector((state) => state.cart)
+
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand='md' collapseOnSelect>
@@ -18,6 +23,15 @@ const Header = () => {
                         <Nav className='ms-auto'>
                             <Link to='/cart' className='custom-link nav-link'>
                                 <FaShoppingCart />Cart
+                                {
+                                    cartItems.length > 0 && (<Badge 
+                                        pill 
+                                        bg='success' 
+                                        style={{marginLeft:'5px'}}>
+
+                                        {cartItems.reduce((a,c)=> a+c.qty, 0)}
+                                        </Badge>)
+                                }
                             </Link>
 
                             <Link to='/login' className='custom-link nav-link'>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,20 +1,32 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from "../components/Rating";
 import { useGetProductDetailsQuery } from "../redux/slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import { addToCart } from "../redux/slices/cartSlice";
+import { useDispatch } from "react-redux";
 
 function ProductScreen() {
     const { id } = useParams();
     const { data: product, isLoading, error } = useGetProductDetailsQuery(id);
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [qty, setQty] = useState(1)
+
+    const addToCartHandler = () => {
+        dispatch(addToCart({...product, qty}))
+        navigate('/cart')
+    }
 
     return (
         <>
             <Link className="my-3 btn btn-light" to={'/'}>Go Back</Link>
 
-            {isLoading ? <Loader/> : error ? (
+            {isLoading ? <Loader /> : error ? (
                 <Message variant='danger'>{error?.data?.message || error.message}</Message>
             ) : (
                 <Row>
@@ -52,11 +64,31 @@ function ProductScreen() {
                                         <Col><strong>{product.countInStock > 0 ? "Available" : "Sold Out"}</strong></Col>
                                     </Row>
                                 </ListGroup.Item>
+
+                                {product.countInStock > 0 && (<ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col><Form.Control
+                                            as='select'
+                                            value={qty}
+                                            onChange={e => setQty(Number(e.target.value))}>
+                                            {[...Array(product.countInStock).keys()].map(x => (
+                                                <option
+                                                    key={x + 1}
+                                                    value={x + 1}>
+                                                    {x + 1}
+                                                </option>
+                                            ))
+                                            }
+                                        </Form.Control></Col>
+                                    </Row>
+                                </ListGroup.Item>)}
                                 <ListGroup.Item>
                                     <Button
                                         type="button"
                                         className="btn-block"
                                         disabled={product.countInStock == 0}
+                                        onClick={addToCartHandler}
                                     > Add to Cart
                                     </Button>
                                 </ListGroup.Item>
